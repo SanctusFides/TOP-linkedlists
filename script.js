e81ff77200c804f85f38640e89dd1e3f3af02fb7
John <jhn.hines@gmail.com>
2023-07-26 15:56:16 -0500

    toString functionality completed

--- script.js
+++ script.js
@@ -9,24 +9,32 @@ class LinkedList {
 
   constructor() {
     this.head = null;
-    this.tail = null;
   }
 
   head() {
     return this.head;
   }
 
-  addFirst(node) {
+  prepend(node) {
     this.head = node;
   }
 
   toString() {
-    let string;
+    let string = "";
+    //setting currentNode to head sets the starting place for moving through the while loop
     let currentNode = this.head;
-    while (currentNode.pointer != null) {
-      string += `(${currentNode.value}) ->`
+    // set a nullcheck on linked list to ensure it has a start
+    if (currentNode == null) {
+      return "No nodes attached to this linked list";
     }
-    string += ` -> null`;
+    // While there is the next nodes pointer set, attach this nodes value to our string
+    // this will NOT include the last node to the string
+    while (currentNode.pointer != null) {
+      string += `(${currentNode.value}) -> `
+      currentNode = currentNode.pointer;
+    }
+    // This attached this first AND OR final node to the string followed by the null info to indicate end
+    string += `(${currentNode.value}) -> null`;
     console.log(string);
   }
 }
@@ -37,7 +45,12 @@ const testNode1 = new Node("test1");
 const testNode2 = new Node("test2");
 testNode1.pointer = testNode2;
 
-testList.addFirst(testNode1);
-console.log("boop");
+const testNode3 = new Node("test3");
+testNode2.pointer = testNode3;
+
+const testNode4 = new Node("test4");
+testNode3.pointer = testNode4;
+
+testList.prepend(testNode1);
 console.log(testList.toString());
 
